cab4a3c63741bb4a25264dd5a789bd0de025a5a0
mylesh-dev <mile.kuzmina@gmail.com>
2022-08-08 21:48:32 +0300

    #1.#2. Update project structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,21 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
-    application
+    kotlin("jvm")           apply false
+    kotlin("multiplatform") apply false
 }
 
-group = "net.murzikov.webchat"
+group   = "net.murzikov.webchat"
 version = "0.0.1"
 
-repositories {
-    mavenCentral()
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
 }
 
-dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.7.0")
-}
-
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "11"
-}
-
-application {
-    mainClass.set("MainKt")
+subprojects {
+    group   = rootProject.group
+    version = rootProject.version
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,19 @@
-
 rootProject.name = "202206-ok-web-chat"
 
+pluginManagement {
+    plugins {
+        val kotlinVersion:  String by settings
+        val kotestVersion:  String by settings
+        val openapiVersion: String by settings
+
+        kotlin("jvm")                  version kotlinVersion  apply false
+        kotlin("multiplatform")        version kotlinVersion  apply false
+        id("io.kotest.multiplatform")      version kotestVersion  apply false
+        kotlin("plugin.serialization") version kotlinVersion  apply false
+
+        id("org.openapi.generator")        version openapiVersion apply false
+
+    }
+}
+
+include("ok-webchat-common")
