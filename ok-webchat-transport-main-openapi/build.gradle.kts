e9bb0f619ed77724e1ea9aa3cb7cd356e24b65fd
mylesh-dev <mile.kuzmina@gmail.com>
2022-08-19 00:09:09 +0300

    #3. Create OpenAPI configuration and module

---
+++ ok-webchat-transport-main-openapi/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    kotlin("jvm")
+    id("org.openapi.generator")
+}
+
+dependencies {
+    val jacksonVersion: String by project
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${jacksonVersion}")
+    testImplementation(kotlin("test-junit"))
+}
+
+sourceSets {
+    main {
+        java.srcDir("$buildDir/generate-resources/main/src/main/kotlin")
+    }
+}
+
+
+/**
+ * Ќастраиваем генерацию здесь
+ */
+openApiGenerate {
+    val openapiGroup = "${rootProject.group}.api.v1"
+    generatorName.set("kotlin") // Ёто и есть активный генератор
+    packageName.set(openapiGroup)
+    apiPackage.set("$openapiGroup.api")
+    modelPackage.set("$openapiGroup.models")
+    invokerPackage.set("$openapiGroup.invoker")
+    inputSpec.set("$rootDir/specs/spec-webchat-v1.yaml")
+
+    /**
+     * «десь указываем, что нам нужны только модели, все остальное не нужно
+     */
+    globalProperties.apply {
+        put("models", "")
+        put("modelDocs", "false")
+    }
+
+    /**
+     * Ќастройка дополнительных параметров из документации по генератору
+     * https://github.com/OpenAPITools/openapi-generator/blob/master/docs/generators/kotlin.md
+     */
+    configOptions.set(
+        mapOf(
+            "dateLibrary" to "string",
+            "enumPropertyNaming" to "UPPERCASE",
+            "serializationLibrary" to "jackson",
+            "collectionType" to "list"
+        )
+    )
+}
+
+tasks {
+    compileKotlin {
+        dependsOn(openApiGenerate)
+    }
+}
+
